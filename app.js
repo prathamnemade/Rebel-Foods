7482d141a9108e9e9370a4eafc58df3de4cb48f2
prathamnemade <prathameshnemade95@gmail.com>
2019-05-14 01:42:44 +0530

    test

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var bodyParser = require('body-parser');
 var cors = require('cors');
 var app = express();
 var server = require('http').createServer(app);
-var io = require('socket.io')(server);
+// var io = require('socket.io')(server);
 const port = process.env.PORT || 2000;
 app.set('port', port);
 // server.listen(1111);
@@ -12,20 +12,20 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 require('./api/models/db');
 
-// socket io
-io.on('connection', function(socket) {
-    console.log('User connected');
-    socket.on('disconnect', function() {
-        console.log('User disconnected');
-    });
-    socket.on('notificationData', function(data) {
-        console.log(data);
-        io.emit('newNotification', { value: true, newOrder: data });
-    });
-    socket.on('doneData', function(data) {
-        io.emit('notifyOrderComplete', data)
-    })
-});
+// // socket io
+// io.on('connection', function(socket) {
+//     console.log('User connected');
+//     socket.on('disconnect', function() {
+//         console.log('User disconnected');
+//     });
+//     socket.on('notificationData', function(data) {
+//         console.log(data);
+//         io.emit('newNotification', { value: true, newOrder: data });
+//     });
+//     socket.on('doneData', function(data) {
+//         io.emit('notifyOrderComplete', data)
+//     })
+// });
 var routesApi = require('./api/routes/index');
 app.use('/', routesApi)
 app.listen(port, () => console.log(`Example app listening on port ` + port))
